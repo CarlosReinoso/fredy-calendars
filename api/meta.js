32835cd40885ed05770ffeb4a66115a598e8c238
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2024-09-13 22:25:08 +0100

    meta log

--- api/meta.js
+++ api/meta.js
@@ -1,6 +1,9 @@
 const puppeteer = require("puppeteer-core");
 const chrome = require("chrome-aws-lambda");
 
+console.log("meta", process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD)
+console.log("AWS_LAMBDA_FUNCTION_VERSION", process.env.AWS_LAMBDA_FUNCTION_VERSION)
+
 module.exports = async (req, res) => {
   try {
     const url = req.query.url;
