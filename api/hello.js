7eeefb49d8e32218adce66c90c533bcec28bd6fd
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2024-09-13 23:45:48 +0100

    bit

--- api/hello.js
+++ api/hello.js
@@ -21,148 +21,148 @@ module.exports = async function (req, res) {
     });
 
     // Click the "Continue with email" button
-    try {
-      await page.waitForSelector('button[aria-label="Continue with email"]', {
-        visible: true,
-        timeout: 10000,
-      });
-      await page.click('button[aria-label="Continue with email"]');
-      console.log("Clicked 'Continue with email' button.");
-    } catch (clickError) {
-      console.error("Error clicking 'Continue with email' button:", clickError);
-      await browser.close();
-      res.statusCode = 500;
-      return res.json({
-        message: "Failed to click 'Continue with email' button.",
-      });
-    }
+    // try {
+    //   await page.waitForSelector('button[aria-label="Continue with email"]', {
+    //     visible: true,
+    //     timeout: 10000,
+    //   });
+    //   await page.click('button[aria-label="Continue with email"]');
+    //   console.log("Clicked 'Continue with email' button.");
+    // } catch (clickError) {
+    //   console.error("Error clicking 'Continue with email' button:", clickError);
+    //   await browser.close();
+    //   res.statusCode = 500;
+    //   return res.json({
+    //     message: "Failed to click 'Continue with email' button.",
+    //   });
+    // }
 
-    // Replace with your Airbnb credentials
-    const email = process.env.AIRBNB_LOGIN;
-    const password = process.env.AIRBNB_PASSWORD;
+    // // Replace with your Airbnb credentials
+    // const email = process.env.AIRBNB_LOGIN;
+    // const password = process.env.AIRBNB_PASSWORD;
 
-    // Log in to Airbnb with email
-    try {
-      await page.waitForSelector('input[type="email"]', {
-        visible: true,
-        timeout: 10000,
-      });
-      await page.type('input[type="email"]', email);
-      await page.waitForSelector('button[data-veloute="submit-btn-cypress"]', {
-        visible: true,
-        timeout: 10000,
-      });
-      await page.evaluate(() => {
-        const button = document.querySelector(
-          'button[data-veloute="submit-btn-cypress"]'
-        );
-        if (button) {
-          button.scrollIntoView();
-          button.focus();
-          button.click();
-        }
-      });
-      console.log("Clicked 'Continue' button using JavaScript evaluate.");
-      await page.waitForSelector('input[type="password"]', {
-        visible: true,
-        timeout: 10000,
-      });
-      await page.type('input[type="password"]', password);
-      await page.waitForSelector('button[type="submit"]', {
-        visible: true,
-        timeout: 10000,
-      });
-      await page.click('button[type="submit"]');
-      console.log("Clicked login button.");
-      await page.waitForNavigation({ waitUntil: "networkidle2" });
-    } catch (loginError) {
-      console.error("Error during login:", loginError);
-      await browser.close();
-      res.statusCode = 500;
-      return res.json({
-        message: "Login failed, please check your credentials and selectors.",
-      });
-    }
+    // // Log in to Airbnb with email
+    // try {
+    //   await page.waitForSelector('input[type="email"]', {
+    //     visible: true,
+    //     timeout: 10000,
+    //   });
+    //   await page.type('input[type="email"]', email);
+    //   await page.waitForSelector('button[data-veloute="submit-btn-cypress"]', {
+    //     visible: true,
+    //     timeout: 10000,
+    //   });
+    //   await page.evaluate(() => {
+    //     const button = document.querySelector(
+    //       'button[data-veloute="submit-btn-cypress"]'
+    //     );
+    //     if (button) {
+    //       button.scrollIntoView();
+    //       button.focus();
+    //       button.click();
+    //     }
+    //   });
+    //   console.log("Clicked 'Continue' button using JavaScript evaluate.");
+    //   await page.waitForSelector('input[type="password"]', {
+    //     visible: true,
+    //     timeout: 10000,
+    //   });
+    //   await page.type('input[type="password"]', password);
+    //   await page.waitForSelector('button[type="submit"]', {
+    //     visible: true,
+    //     timeout: 10000,
+    //   });
+    //   await page.click('button[type="submit"]');
+    //   console.log("Clicked login button.");
+    //   await page.waitForNavigation({ waitUntil: "networkidle2" });
+    // } catch (loginError) {
+    //   console.error("Error during login:", loginError);
+    //   await browser.close();
+    //   res.statusCode = 500;
+    //   return res.json({
+    //     message: "Login failed, please check your credentials and selectors.",
+    //   });
+    // }
 
-    // Navigate to the calendar page after successful login
-    await page.goto(
-      "https://www.airbnb.co.uk/multicalendar/1228348447908449096/availability-settings/",
-      { waitUntil: "networkidle2" }
-    );
+    // // Navigate to the calendar page after successful login
+    // await page.goto(
+    //   "https://www.airbnb.co.uk/multicalendar/1228348447908449096/availability-settings/",
+    //   { waitUntil: "networkidle2" }
+    // );
 
-    // Scroll down to ensure all content is loaded
-    await page.evaluate(async () => {
-      await new Promise((resolve) => {
-        const scrollInterval = setInterval(() => {
-          window.scrollBy(0, window.innerHeight);
-          if (
-            window.scrollY + window.innerHeight >=
-            document.body.scrollHeight
-          ) {
-            clearInterval(scrollInterval);
-            resolve();
-          }
-        }, 200); // Adjust this interval to control the scroll speed
-      });
-    });
+    // // Scroll down to ensure all content is loaded
+    // await page.evaluate(async () => {
+    //   await new Promise((resolve) => {
+    //     const scrollInterval = setInterval(() => {
+    //       window.scrollBy(0, window.innerHeight);
+    //       if (
+    //         window.scrollY + window.innerHeight >=
+    //         document.body.scrollHeight
+    //       ) {
+    //         clearInterval(scrollInterval);
+    //         resolve();
+    //       }
+    //     }, 200); // Adjust this interval to control the scroll speed
+    //   });
+    // });
 
-    // Wait a moment after scrolling to ensure elements have settled
-    await new Promise((resolve) => setTimeout(resolve, 2000)); // Use a native delay
+    // // Wait a moment after scrolling to ensure elements have settled
+    // await new Promise((resolve) => setTimeout(resolve, 2000)); // Use a native delay
 
-    try {
-      console.log("🚀 Attempting to click the Refresh button...");
-      // Use page.evaluate to find the button and simulate a more realistic click
-      const refreshClicked = await page.evaluate(() => {
-        // Locate the button by class name
-        const buttons = document.querySelectorAll("button.l1ovpqvx");
-        console.log("🚀 ~ Buttons found:", buttons);
-        let clicked = false;
-        buttons.forEach((button) => {
-          // Check if the button contains the text "Refresh"
-          if (button.innerText.includes("Refresh")) {
-            // Ensure the button is enabled
-            if (!button.disabled && !button.getAttribute("aria-disabled")) {
-              // Scroll the button into view
-              button.scrollIntoView({ behavior: "smooth", block: "center" });
-              // Simulate a more realistic user interaction
-              const rect = button.getBoundingClientRect();
-              const clickEvent = new MouseEvent("click", {
-                bubbles: true,
-                cancelable: true,
-                view: window,
-                clientX: rect.left + rect.width / 2,
-                clientY: rect.top + rect.height / 2,
-              });
-              // Dispatch the click event
-              button.dispatchEvent(clickEvent);
-              clicked = true;
-              console.log("🚀 Refresh button clicked successfully.");
-            } else {
-              console.log("🚀 Refresh button is disabled or not clickable.");
-            }
-          }
-        });
-        return clicked;
-      });
+    // try {
+    //   console.log("🚀 Attempting to click the Refresh button...");
+    //   // Use page.evaluate to find the button and simulate a more realistic click
+    //   const refreshClicked = await page.evaluate(() => {
+    //     // Locate the button by class name
+    //     const buttons = document.querySelectorAll("button.l1ovpqvx");
+    //     console.log("🚀 ~ Buttons found:", buttons);
+    //     let clicked = false;
+    //     buttons.forEach((button) => {
+    //       // Check if the button contains the text "Refresh"
+    //       if (button.innerText.includes("Refresh")) {
+    //         // Ensure the button is enabled
+    //         if (!button.disabled && !button.getAttribute("aria-disabled")) {
+    //           // Scroll the button into view
+    //           button.scrollIntoView({ behavior: "smooth", block: "center" });
+    //           // Simulate a more realistic user interaction
+    //           const rect = button.getBoundingClientRect();
+    //           const clickEvent = new MouseEvent("click", {
+    //             bubbles: true,
+    //             cancelable: true,
+    //             view: window,
+    //             clientX: rect.left + rect.width / 2,
+    //             clientY: rect.top + rect.height / 2,
+    //           });
+    //           // Dispatch the click event
+    //           button.dispatchEvent(clickEvent);
+    //           clicked = true;
+    //           console.log("🚀 Refresh button clicked successfully.");
+    //         } else {
+    //           console.log("🚀 Refresh button is disabled or not clickable.");
+    //         }
+    //       }
+    //     });
+    //     return clicked;
+    //   });
 
-      if (refreshClicked) {
-        console.log(
-          "Refresh button clicked successfully using enhanced simulation."
-        );
-      } else {
-        console.log("Failed to click the Refresh button, no action taken.");
-        throw new Error("Refresh button not found or could not be clicked.");
-      }
+    //   if (refreshClicked) {
+    //     console.log(
+    //       "Refresh button clicked successfully using enhanced simulation."
+    //     );
+    //   } else {
+    //     console.log("Failed to click the Refresh button, no action taken.");
+    //     throw new Error("Refresh button not found or could not be clicked.");
+    //   }
 
-      // Wait for the page to update or any indication of completion
-      await new Promise((resolve) => setTimeout(resolve, 3000)); // Adjust this timeout based on expected action delay
-    } catch (refreshError) {
-      console.error("Error clicking the Refresh button:", refreshError);
-      res.statusCode = 500;
-      return res.json({
-        message: "Error clicking the Refresh button.",
-      });
-    }
+    //   // Wait for the page to update or any indication of completion
+    //   await new Promise((resolve) => setTimeout(resolve, 3000)); // Adjust this timeout based on expected action delay
+    // } catch (refreshError) {
+    //   console.error("Error clicking the Refresh button:", refreshError);
+    //   res.statusCode = 500;
+    //   return res.json({
+    //     message: "Error clicking the Refresh button.",
+    //   });
+    // }
 
     // Close the browser
     await browser.close();
