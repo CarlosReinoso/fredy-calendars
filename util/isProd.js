5a5b532b7728399168587c9b3d9b46fa7a4e5ef2
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2024-09-15 17:59:57 +0100

    add user prefences

--- util/isProd.js
+++ util/isProd.js
@@ -1,4 +1,5 @@
-const isProd = process.env.NODE_ENV == "production" ? true : false;
+const isProd = `process.env.NODE_ENV` == "production" ? true : false;
+console.log("🚀 ~ process.env.NODE_ENV:", process.env.NODE_ENV)
 
 module.exports = {
   isProd,
